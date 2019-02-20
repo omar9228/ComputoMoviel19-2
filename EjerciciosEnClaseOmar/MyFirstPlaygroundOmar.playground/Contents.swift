e1fe1c3523363cd6061ccf116e92a91c2122adfd
omar9228 <47132488+omar9228@users.noreply.github.com>
2019-02-20 11:14:14 -0600

    Add files via upload

---
+++ EjerciciosEnClaseOmar/MyFirstPlaygroundOmar.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+var status = "😃"
+print(status)
+
+
+
+var sumas = (3 + 2)
+print(sumas)
+
+let favoriteBook = "The Sun Also Rises"
+str = "Hello, Omar"
+print(str)
+
+
+3 + 4
+
+//Comentario!
+
+/*Bloque
+ de
+ comentarios
+ */
